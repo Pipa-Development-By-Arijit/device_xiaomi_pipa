618c240425aac2f9fa68f954c0380e02de4a9980
Arijit78 <sahaarijit2007@gmail.com>
2024-08-22 16:23:23 +0000

    pipa: Init vendorsetup.sh
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Clone Kernel Tree
+git clone https://github.com/Pipa-Development-By-Arijit/kernel_xiaomi_sm8250.git -b udc kernel/xiaomi/sm8250
+
+# Clone Commom Device Tree
+git clone https://github.com/Pipa-Development-By-Arijit/device_xiaomi_sm8250-common.git -b udc device/xiaomi/sm8250-common
+
+# Clone Vendor Tree
+git clone https://github.com/Pipa-Development-By-Arijit/vendor_xiaomi_pipa.git -b udc vendor/xiaomi/pipa
+
+# Clone Vendor Common Tree
+git clone https://github.com/Pipa-Development-By-Arijit/vendor_xiaomi_sm8250-common.git -b udc vendor/xiaomi/sm8250-common
+
+# Clone Hardware Xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
